62e40841a715e53b91404cd97a36bcfe66ec31f5
Sebastian Mira <juanmira720@gmail.com>
2022-06-24 19:19:02 -0500

    adding constraint rules unique in user

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,15 +5,28 @@ namespace Ecommerce.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<User> User { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<HIstorial> HIstorials { get; set; }
-        public DbSet<Score> Scores { get; set; }
-        public DbSet<Role> Roles { get; set; }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<HIstorial>? HIstorials { get; set; }
+        public DbSet<Score>? Scores { get; set; }
+        public DbSet<Role>? Roles { get; set; }
+
+        // overrides
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().
+                HasIndex(u => u.Username).
+                IsUnique();
+            builder.Entity<User>().
+                HasIndex(u => u.Email).
+                IsUnique();
+        }
     }
 }
